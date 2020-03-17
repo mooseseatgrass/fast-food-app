bdf8e55dd1542db147b377dcfeb8d1d85a50b9e9
mooseseatgrass <skylover29@gmail.com>
2020-03-17 14:39:04 -0400

    added notes

---
+++ scripts/app.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+// example from calc quiz
+// function superCalc(){
+//     var input1 = parseInt(document.getElementById("input1").value);
+//     var input2 = parseInt(document.getElementById("input2").value);
+//     var division =  (input1 / input2);
+//     division.toFixed(2);
+
+//     if (input2 !== 0){
+//     console.log(division.toFixed(2));
+//     } else {
+//         console.log("Cannot divide by 0");
+//     }
+//     document.querySelector("#showOrder").innerHTML = division.toFixed(2);
+// };
